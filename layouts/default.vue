35f3e6cfe61bba8bb530088a89f40f1cd0c2b5a9
Thomas WITZ <thomas.witz@epita.fr>
2022-08-21 11:35:06 +0200

    feat: add default and 404 error layouts

---
+++ layouts/default.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+      <Header />
+      <Nuxt />
+  </div>
+</template>
+
+<script>
+import Header from '~/components/Header.vue';
+
+export default {
+	components: {
+        Header
+}}
+</script>
+
+<style>
+html {
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+body, html {
+  width: 100%;
+  height: 100%;
+  margin: 0px;
+}
+
+body {
+  background: radial-gradient(ellipse at top, hsl(214, 47%, 23%), hsl(237, 49%, 15%));
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  text-align: center;
+  /* min-height: 200px; */
+  align-items: center;
+}
+</style>

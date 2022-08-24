e30e78a7f66b5b3a9af8cf82ab57a312f4654a53
Thomas WITZ <thomas.witz@epita.fr>
2022-08-24 14:37:48 +0200

    fix: simplify dynamic load of svg components for buttons

--- components/GameButton.vue
+++ components/GameButton.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="game-button" :style="cssProps" @click="onClick">
     <div class="white-center">
-      <img class="image" :src="buttonImage">
+      <img class="image" :src="require(`~/assets/icon-${type.toLowerCase()}.svg`)">
     </div>
   </div>
 </template>
@@ -24,14 +24,6 @@ export default {
     }
   },
   computed: {
-    buttonImage () {
-      // could be replaced by: <img :src="require(`~/assets/icon-${type.toLowerCase()}.svg`)">
-      if (!this.type) {
-        return
-      }
-      const fileName = this.type.toLowerCase()
-      return require(`~/assets/icon-${fileName}.svg`)
-    },
     cssProps () {
       return {
         '--contour-color-a': this.colorA,
@@ -42,10 +34,12 @@ export default {
   },
   methods: {
     onClick () {
-      console.log(this.type + ' clicked')
-      // this.$emit('test');
+      this.$emit('onClick', this.type)
     },
     darkenHsl (color) {
+      if (color.charAt(0) !== 'h') {
+        return
+      }
       const splited = color.split('%')
       let lightness = splited[splited.length - 2].substring(1)
       lightness = parseInt(lightness) - 15
@@ -84,8 +78,8 @@ export default {
 
 @media only screen and (max-width: 576px) {
     .image {
-        width: 60%;
-        height: 60%;
+        width: 55%;
+        height: 55%;
     }
 }
 </style>
